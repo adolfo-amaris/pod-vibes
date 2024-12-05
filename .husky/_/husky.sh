e1b254382441e838f2025bbc740e02523c4c220c
Adolfo Amaris <adolfo.amaris@insttantt.com>
2024-12-06 00:01:22 +0100

    feat(core): agregar soporte para nuevos hooks

--- .husky/_/husky.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Husky script base
-if [ -z "$husky_skip_init" ]; then
-  hook_name="$(basename "$0")"
-  hook_path=".husky/$hook_name"
-  if [ -f "$hook_path" ]; then
-    . "$hook_path"
-  fi
-fi
